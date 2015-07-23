17399068e1907dfb971b5248ba19b45c720c167a
Philipp Haussleiter <philipp@haussleiter.de>
2015-07-23 02:48:23 +0200

    fixing the evil space in filenames bug

--- app/helpers/SystemHelper.java
+++ app/helpers/SystemHelper.java
@@ -3,15 +3,20 @@ package helpers;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
+import com.typesafe.config.ConfigFactory;
+
 public class SystemHelper {
+	private final static String SYSTEM_bash_BIN = ConfigFactory.load().getString("system.bash.bin");
+
 	private SystemHelper() {
 	}
 
 	public static String runCommand(String cmd) {
 		StringBuilder sb = new StringBuilder();
 		try {
+			ProcessBuilder pb = new ProcessBuilder(SYSTEM_bash_BIN, "-c", cmd);
 			String line;
-			Process p = Runtime.getRuntime().exec(cmd);
+			Process p = pb.start();
 			BufferedReader input = new BufferedReader(new InputStreamReader(p.getInputStream()));
 			while ((line = input.readLine()) != null) {
 				sb.append(line.trim()).append("\n");
